db5c124afb81c06f6a886635c8613bbb9c68fa8c
soufianenajim99 <soufianenajim100@gmail.com>
2023-12-08 20:06:26 +0100

    groupsfin

--- app/views/admin/Users.php
+++ app/views/admin/Users.php
@@ -43,7 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $ville=$_POST['ville'];
   $quartier=$_POST['quartier'];
   $agency=$_POST['agency'];
-  $role = $_POST['role'];
+  $roles=$_POST['roles'];
+  $role=$roles[0];
   $postal=$_POST['postal'];
   
   if (!preg_match("/^[A-Za-z\s]+$/", $username)) {
@@ -115,7 +116,7 @@ if (empty(array_filter($errors))) {
 <?php include './aside.php'?>
 
 <!-- ============ Content ============= -->
-<div class="p-6 bg-white m-5">
+<div class="md:p-6 bg-white md:m-5">
     <div class="flex items-center justify-between">
         <div>
             <h3 class="text-orange-600 text-3xl font-bold tracking-widest mb-2">
@@ -124,16 +125,17 @@ if (empty(array_filter($errors))) {
             <p class="text-xl">Our Users around The world</p>
         </div>
         <div>
-            <button class="bg-slate-900 text-white w-[160px] h-[50px] rounded-md" id="addBank">
+            <button class="bg-[#186F65] text-white w-[160px] h-[50px] rounded-md" id="addBank">
                 Add User
             </button>
         </div>
     </div>
     <!-- ========== table Banks ======== -->
-    <div class="rounded-lg overflow-hidden mt-10">
-        <table class="w-full table-auto" id="table1">
-            <thead class="">
-                <tr class="bg-slate-900 text-white h-[60px]">
+    <div class="hidden md:block  rounded-lg overflow-hidden mt-10">
+        <table class=" 
+           w-full   " id="table1">
+            <thead class="  sm:w-full">
+                <tr class="bg-[#186F65] text-white h-[60px]">
                     <th class="">ID</th>
                     <th class="">Username</th>
                     <th class="">Role</th>
@@ -142,24 +144,89 @@ if (empty(array_filter($errors))) {
                     <th class="">Actions</th>
                 </tr>
             </thead>
-            <tbody>
-
-
+            <tbody class="sm:w-full">
 
                 <?php 
               foreach($data_users as $duser) {
               ?>
-                <tr class="h-[50px]">
+                <tr class=" pt-10 sm:pt-0  w-full ">
 
-                    <td class="text-center"><?php echo $duser->userId ?></td>
-                    <td class="text-center"><?php echo $duser->username ?></td>
-                    <td class="text-center"><?php echo $duser->roleName ?></td>
-                    <td class="text-center"><?php echo $duser->email ?></td>
-                    <td class="text-center">
+                    <td class=" sm:text-center text-right">
+                        <?php echo $duser->userId ?></td>
+                    <td class=" sm:text-center text-right">
+                        <?php echo $duser->username ?>
+                    </td>
+                    <td class=" sm:text-center text-right">
+                        <?php echo $duser->roleName ?>
+                    </td>
+                    <td class=" sm:text-center 
+                             text-right">
+                        <?php echo $duser->email ?></td>
+                    <td class="  sm:text-center  text-right">
+                        <button class="bg-[#B5CB99] text-white w-[35px] h-[35px] rounded-md">
+                            <a href="../../../app/views/users/updateUser.php?user_id=<?= $duser->userId;?>"> <i
+                                    class="fa-solid fa-pen " style="color:#186F65"></i></a>
+
+
+                        </button>
+                        <button class="bg-[#B5CB99] text-white w-[35px] h-[35px] rounded-md">
+                            <a href="../../../app/views/users/deleteUser.php?user_id=<?= $duser->userId;?>"><i
+                                    class="fa-solid fa-trash " style="color:#186F65"></i></a>
+
+                        </button>
+
+                        <button class="bg-[#B5CB99] text-white w-[35px] h-[35px] rounded-md">
+                            <a href="../../../app/views/users/UserAcc.php?user_id=<?= $duser->userId;?>"><i
+                                    class="fa-solid fa-file" style="color:#186F65"></i></a>
+
+                        </button>
+                    </td>
+
+                </tr>
+                <?php 
+              }
+              ?>
+
+            </tbody>
+        </table>
+    </div>
+    <!-- ========== table Banks-mobile ======== -->
+    <div class="block md:hidden rounded-lg overflow-hidden mt-10">
+        <table class=" block w-full  border-2 sm:border-0  " id="table2">
+            <thead class="hidden">
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="block  w-full">
+                <?php 
+              foreach($data_users as $duser) {
+              ?>
+                <tr class="block pt-10 sm:pt-0   w-full ">
+
+                    <td data-label="id"
+                        class="border-b before:content-['id']  before:absolute before:left-0 before:w-1/2 before:font-bold before:text-left before:pl-2 sm:before:hidden sm:text-center block    text-right">
+                        <?php echo $duser->userId ?></td>
+                    <td data-label="username" class="border-b before:content-['username'] before:absolute before:left-0 before:w-1/2 before:font-bold before:text-left before:pl-2 block  sm:before:hidden sm:text-center 
+                             text-right">
+                        <?php echo $duser->username ?>
+                    </td>
+                    <td data-label="Role" class="border-b before:content-['Role'] before:absolute before:left-0 before:w-1/2 before:font-bold before:text-left before:pl-2 sm:before:hidden block  sm:text-center 
+                            text-right">
+                        <?php echo $duser->roleName ?>
+                    </td>
+                    <td data-label="Email" class="border-b before:content-['Email'] before:absolute before:left-0 before:w-1/2 before:font-bold before:text-left before:pl-2  sm:before:hidden block sm:text-center 
+                             text-right">
+                        <?php echo $duser->email ?></td>
+                    <td data-label="ACtion"
+                        class="border-b before:content-['action'] before:absolute before:left-0 before:w-1/2 before:font-bold before:text-left before:pl-2  sm:before:hidden  sm:text-center block    text-right">
                         <button class="bg-slate-900 text-white w-[35px] h-[35px] rounded-md">
                             <a href="../../../app/views/users/updateUser.php?user_id=<?= $duser->userId;?>"> <i
                                     class="fa-solid fa-pen"></i></a>
-
 
                         </button>
                         <button class="bg-slate-900 text-white w-[35px] h-[35px] rounded-md">
@@ -171,13 +238,6 @@ if (empty(array_filter($errors))) {
                         <button class="bg-slate-900 text-white w-[35px] h-[35px] rounded-md">
                             <a href="../../../app/views/users/UserAcc.php?user_id=<?= $duser->userId;?>"><i
                                     class="fa-solid fa-file"></i></a>
-
-                        </button>
-
-
-                        <button class="bg-slate-900 text-white w-[35px] h-[35px] rounded-md">
-                            <a href="../../../app/views/users/userPerm.php?id=<?= $duser->userId;?>"><i
-                                    class="fa-solid fa-gears"></i></a>
 
                         </button>
                     </td>
@@ -193,25 +253,25 @@ if (empty(array_filter($errors))) {
     <!-- ============ Form to add New Users ========= -->
     <div>
         <form action="" method="post"
-            class="absolute top-[50%] left-[20%] translate-y-[-50%] bg-white p-5 w-[1000px] rounded-md shadow-sm z-50 hidden"
+            class="absolute  -left-20 top-[50%] sm:left-[20%] translate-y-[-50%] bg-white p-5 w-[500px] sm:w-[1000px] rounded-md shadow-sm z-50 hidden"
             id="Add">
             <h1 class="text-center font-semibold text-3xl my-5">
                 Add new User
             </h1>
             <div class="flex gap-5">
                 <div class="w-[50%]">
-                    <label for="" class="text-xl">Username</label>
+                    <label for="" class="text-sm sm:text-xl">Username</label>
                     <input type="text" name="username"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                         placeholder="Enter Username " />
                     <?php if (!empty($errors['username'])) : ?>
                     <div class="text-red-500"><?php echo $errors['username']; ?></div>
                     <?php endif; ?>
                 </div>
                 <div class="w-[50%]">
-                    <label for="" class="text-xl">Email</label>
+                    <label for="" class="text-sm sm:text-xl">Email</label>
                     <input type="email" name="email"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                         placeholder="Enter email " />
                     <?php if (!empty($errors['email'])) : ?>
                     <div class="text-red-500"><?php echo $errors['email']; ?></div>
@@ -222,18 +282,18 @@ if (empty(array_filter($errors))) {
             <!-- gender -->
             <div class="flex gap-5">
                 <div class="w-[50%]">
-                    <label for="" class="text-xl">Gendre</label>
+                    <label for="" class="text-sm sm:text-xl">Gendre</label>
                     <select name="gendre" id="gendre"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100">
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100">
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
 
                     </select>
                 </div>
                 <div class="w-[50%]">
-                    <label for="" class="text-xl">Phone</label>
+                    <label for="" class="text-sm sm:text-xl">Phone</label>
                     <input type="tel" name="phone"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                         placeholder="Enter Your Phone " />
                     <?php if (!empty($errors['phone'])) : ?>
                     <div class="text-red-500"><?php echo $errors['phone']; ?></div>
@@ -245,43 +305,43 @@ if (empty(array_filter($errors))) {
 
             <div class="flex gap-5">
                 <div class="w-[50%]">
-                    <label for="" class="text-xl">Password</label>
+                    <label for="" class="text-sm sm:text-xl">Password</label>
                     <input type="password" name="password" placeholder="Enter Password"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100" />
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100" />
                     <?php if (!empty($errors['password'])) : ?>
                     <div class="text-red-500"><?php echo $errors['password']; ?></div>
                     <?php endif; ?>
                 </div>
                 <div class="w-[50%]">
-                    <label for="newpassword" class="text-xl">Confirm Password</label>
+                    <label for="newpassword" class="text-sm sm:text-xl">Confirm Password</label>
                     <input type="password" name="newpassword" placeholder="Confirm your Password"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100" />
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100" />
                 </div><?php if (!empty($errors['newpassword'])) : ?>
                 <div class="text-red-500"><?php echo $errors['newpassword']; ?></div>
                 <?php endif; ?>
             </div>
             <div class="flex gap-5">
                 <div class="w-full">
-                    <label for="" class="text-xl">Rue</label>
+                    <label for="" class="text-sm sm:text-xl">Rue</label>
                     <input type="text" name="rue"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                         placeholder="Enter Rue " /><?php if (!empty($errors['rue'])) : ?>
                     <div class="text-red-500"><?php echo $errors['rue']; ?></div>
                     <?php endif; ?>
                 </div>
                 <div class="w-full">
-                    <label for="" class="text-xl">Ville</label>
+                    <label for="" class="text-sm sm:text-xl">Ville</label>
                     <input type="text" name="ville"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                         placeholder="Enter Ville" /><?php if (!empty($errors['ville'])) : ?>
                     <div class="text-red-500"><?php echo $errors['ville']; ?></div>
                     <?php endif; ?>
                 </div>
             </div>
             <div class="w-full">
-                <label for="" class="text-xl">Quartier</label>
+                <label for="" class="text-sm sm:text-xl">Quartier</label>
                 <input type="text" name="quartier"
-                    class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                    class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                     placeholder="Enter  Quartier " /><?php if (!empty($errors['quartier'])) : ?>
                 <div class="text-red-500"><?php echo $errors['quartier']; ?></div>
                 <?php endif; ?>
@@ -289,9 +349,9 @@ if (empty(array_filter($errors))) {
 
             <div class="flex gap-4">
                 <div class="w-[33%]">
-                    <label for="" class="text-xl">Agency</label>
+                    <label for="" class="text-sm sm:text-xl">Agency</label>
                     <select name="agency" id=""
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100">
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100">
 
                         <?php
             foreach ($agency_list as $user) {
@@ -303,18 +363,23 @@ if (empty(array_filter($errors))) {
                     </select>
                 </div>
                 <div class="w-[33%]">
-                    <label for="" class="text-xl">Role</label>
-                    <select name="role" id=""
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100">
+                    <label for="" class="form-label text-sm sm:text-xl">Roles</label>
+                    <select name="roles[]" id=""
+                        class="roles form-select roles block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        multiple>
+
+                        <?php if ($_SESSION["role"] === "admin") { ?>
                         <option value="admin">Admin</option>
+                        <option value="subadmin">subadmin</option>
+                        <?php } ?>
                         <option value="client">Client</option>
 
                     </select>
                 </div>
                 <div class="w-[33%]">
-                    <label for="" class="text-xl">Code Postal</label>
+                    <label for="" class="text-sm sm:text-xl">Code Postal</label>
                     <input type="text" name="postal"
-                        class="block w-full py-3 text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
+                        class="block w-full py-3 text-sm sm:text-xl px-1 placeholder:text-lg my-2 outline-none border-none bg-gray-100"
                         placeholder="Enter Code postal " /><?php if (!empty($errors['postal'])) : ?>
                     <div class="text-red-500"><?php echo $errors['postal']; ?></div>
                     <?php endif; ?>
@@ -323,7 +388,7 @@ if (empty(array_filter($errors))) {
 
             <div>
                 <input type="submit" name="submit" value="Envoyer"
-                    class="block w-full py-3 text-white text-xl px-1 cursor-pointer mt-5 outline-none border-none bg-slate-900" />
+                    class="block w-full py-3 text-white text-sm sm:text-xl px-1 cursor-pointer mt-5 outline-none border-none bg-slate-900" />
             </div>
         </form>
     </div>
@@ -455,6 +520,10 @@ if (empty(array_filter($errors))) {
 <script>
 $(document).ready(function() {
     $('#table1').DataTable();
+});
+$(document).ready(function() {
+    $('#table2').DataTable();
+
 });
 </script>
 
